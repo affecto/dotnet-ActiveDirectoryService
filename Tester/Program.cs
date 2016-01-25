12985ff773fdc4456b96ba7b538f730a82c02e40
johannesvaltonen <johannes.valtonen@affecto.com>
2016-01-25 15:28:18 +0200

    Clean up.

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -9,9 +9,9 @@ namespace Affecto.ActiveDirectoryService.Tester
         static void Main()
         {
             IActiveDirectoryService service = ActiveDirectoryServiceFactory.CreateActiveDirectoryService(ConfigurationManager.AppSettings["domainPath"]);
-            List<IPrincipal> principals = service.GetGroupMembers("turku-list", true, new List<string> { "extensionAttribute8" }).ToList();
-            IPrincipal principal = service.GetUser("valtojoh", new List<string> { "extensionAttribute8" });
-            bool isGroupMember = service.IsGroupMember("valtojoh", "turku-list");
+            List<IPrincipal> principals = service.GetGroupMembers("", true, new List<string> {  }).ToList();
+            IPrincipal principal = service.GetUser("", new List<string> { });
+            bool isGroupMember = service.IsGroupMember("", "");
         }
     }
 }
