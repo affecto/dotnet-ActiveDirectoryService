aa1b5ad2b862c0c3d98b41e15b6aa298cdef63b2
johannesvaltonen <johannes.valtonen@affecto.com>
2016-11-30 15:09:58 +0200

    Added AccountNameComparer class.

---
+++ ActiveDirectoryService/AccountNameComparer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Affecto.ActiveDirectoryService
+{
+    public class AccountNameComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string accountName1, string accountName2)
+        {
+            if (accountName1 == null)
+            {
+                return (accountName2 == null);
+            }
+
+            if (accountName2 == null)
+            {
+                return false;
+            }
+
+            return GetAccountNameWithoutDomain(accountName1).Equals(GetAccountNameWithoutDomain(accountName2), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int GetHashCode(string accountName)
+        {
+            if (accountName == null)
+            {
+                return 0;
+            }
+
+            return GetAccountNameWithoutDomain(accountName).GetHashCode();
+        }
+
+        private static string GetAccountNameWithoutDomain(string accountName)
+        {
+            if (accountName == null)
+            {
+                return null;
+            }
+
+            if (accountName.Contains(@"\"))
+            {
+                return accountName.Split('\\')[1];
+            }
+
+            return accountName;
+        }
+    }
+}
